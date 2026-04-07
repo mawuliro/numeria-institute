82d009f86e8799274d30d6f86ed00b592a35178e
Mawulikplimi Roland Hounkpe <mawuliro2001@gmail.com>
2026-04-07 22:37:54 +0000

    Add SEO improvements: meta descriptions, Open Graph, Twitter cards, structured data, favicon, canonical URLs, sitemap

--- numeria_project/urls.py
+++ numeria_project/urls.py
@@ -1,8 +1,52 @@
-from django.urls import path, include
+from django.urls import path, include, reverse
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import Sitemap
 from pages import views
+from cours.models import Cours
+from blog.models import Article
+
+
+class StaticSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.8
+
+    def items(self):
+        return ['accueil', 'a_propos', 'contact', 'cours:catalogue']
+
+    def location(self, item):
+        return reverse(item)
+
+
+class CoursSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.9
+
+    def items(self):
+        return Cours.objects.filter(est_publie=True)
+
+    def lastmod(self, obj):
+        return obj.date_modification
+
+
+class ArticleSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.7
+
+    def items(self):
+        return Article.objects.filter(est_publie=True)
+
+    def lastmod(self, obj):
+        return obj.date_modification
+
+
+sitemaps = {
+    'static': StaticSitemap,
+    'cours': CoursSitemap,
+    'blog': ArticleSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,6 +59,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('comptes/', include('comptes.urls')),
     path('paiements/', include('paiements.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
 
 if settings.DEBUG:
