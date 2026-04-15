d185fb860a561e18e272c34f6d323b6ac29a9602
Mawulikplimi Roland Hounkpe <mawuliro2001@gmail.com>
2026-04-15 01:36:25 +0000

    I have done

--- numeria_project/settings.py
+++ numeria_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'django.contrib.sitemaps',
     'cloudinary',           # ← NOUVEAU : Doit être avant cloudinary_storage
     'cloudinary_storage',   # ← NOUVEAU : Doit être avant staticfiles
