d6b3353b3987567442fd40dde012b19e859b2be8
Mawulikplimi Roland Hounkpe <mawuliro2001@gmail.com>
2026-05-31 18:36:20 +0000

    Force cours.models import in AppConfig.ready for proxy model registration

--- cours/apps.py
+++ cours/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class CoursConfig(AppConfig):
     name = 'cours'
+
+    def ready(self):
+        # Ensure models are loaded during app setup so legacy proxy aliases are registered.
+        from . import models  # noqa: F401
